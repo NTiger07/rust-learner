4ff3a30015736403e36f14b07ceffc23166d46b0
NTiger07 <falaleru@gmail.com>
2026-01-01 22:42:32 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// Primitive Data Types
+// int, float, bool, char
+
+// Compound Data Types
+//arrays, tuples, slices, strings
+
+fn main() {
+    let x: i32 = -80;
+    let y: u64 = 80;
+
+    let is_snowing: bool = true;
+    let letter: char = 'a';
+
+    let numbers: [i32; 5] = [1,2,3,4,5];
+}
